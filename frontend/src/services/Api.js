90563dade4b72766e701877730aac2aeb4892c55
palakthakur341 <thakurpalak679@gmail.com>
2026-07-25 15:44:45 +0530

    Fix dependencies

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://page-pulse-backend-618k.onrender.com/api",
 });
 
 export default api;
